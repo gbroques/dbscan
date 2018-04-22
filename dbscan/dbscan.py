eefa616eb7f70b151898032be2b057f828194f7a
G Roques <groques360@gmail.com>
2018-04-22 07:13:51 -0500

    Add function get distances from other points

--- dbscan/dbscan.py
+++ dbscan/dbscan.py
@@ -1,3 +1,75 @@
+from itertools import repeat
+from itertools import chain
+from numpy import ndarray
+from scipy.spatial.distance import pdist
+
+
 class DBSCAN:
-    """Density-Based Spatial Clustering of Applications with Noise."""
-    pass
+    """Density-Based Spatial Clustering of Applications with Noise (DBSCAN)
+
+    Args:
+        eps: The maximum distance between two samples for them to be considered in the same neighborhood.
+        min_samples: The number of samples in a neighborhood for a point to be considered a core point.
+                           This includes the point itself.
+
+    Attributes:
+        core_sample_indices_ (list): Indices of core samples.
+                                     Shape [n_core_samples]
+        components_ (list): Copy of each core sample found by training.
+                            Shape [n_core_samples, n_features]
+        labels_ (list): Cluster labels for each point in the dataset given to fit().
+                        Noisy samples are given the label -1.
+                        Shape [n_samples]
+    """
+
+    def __init__(self, eps: float = 0.5, min_samples: int = 5):
+        self._eps = eps
+        self._min_samples = min_samples
+        self.core_sample_indices_ = None
+        self.components_ = None
+        self.labels_ = None
+
+    def fit(self, data: ndarray) -> 'DBSCAN':
+        """Perform DBSCAN clustering on features.
+
+        Args:
+            data: List of features.
+
+        Returns:
+            self
+        """
+        distance_matrix = pdist(data)
+        return self
+
+
+def get_distances_from_other_points(i: int, n: int, distance_matrix: ndarray):
+    """Get distances from point i to other points in the dataset.
+
+    Args:
+        i: The index of the point.
+        n: The number of points.
+        distance_matrix: Condensed distance matrix.
+
+    Returns:
+        Distance from point i to other points in the dataset.
+    """
+    square_indices = zip(repeat(i, n), chain(range(0, i), range(i + 1, n)))
+    condensed_indices = [square_to_condensed(*square_index, n) for square_index in square_indices]
+    return distance_matrix.take(condensed_indices)
+
+
+def square_to_condensed(i: int, j: int, n: int):
+    """Maps a square index to a condensed index for a given matrix position (i, j).
+
+    Args:
+        i: Index i.
+        j: Index j.
+        n: The dimension of the matrix.
+
+    Returns:
+        Condensed index.
+    """
+    assert i != j, 'No diagonal elements in condensed matrix'
+    if i < j:
+        i, j = j, i
+    return n * j - j * (j + 1) / 2 + i - 1 - j
--- test/dbscan_test.py
+++ test/dbscan_test.py
@@ -1,13 +1,73 @@
 import unittest
 
+import numpy as np
+from numpy import ndarray
+from scipy.spatial.distance import pdist
+
 from dbscan import DBSCAN
+from dbscan.dbscan import get_distances_from_other_points
 
 
 class DBSCANTest(unittest.TestCase):
 
+    @staticmethod
+    def get_two_clusters() -> ndarray:
+        """Two clusters in a 2-dimensional plane.
+        ^
+        | *        o
+        |            o
+        | x            o
+        |   x
+        |     x
+        +-------------->
+        x - Denotes cluster 1
+        o - Denotes cluster 2
+        * - Denotes noise point
+        """
+        return np.array([[1, 3], [2, 2], [3, 1],
+                         [6, 8], [7, 7], [8, 6],
+                         [1, 8]])
+
     def test_fit(self):
-        dbscan = DBSCAN()
-        pass
+        expected_labels = np.array([0, 0, 0, 1, 1, 1, -1])
+        expected_core_sample_indices = np.array([1, 4])
+        expected_components = np.array([[2, 2], [7, 7]])
+        data = self.get_two_clusters()
+        dbscan = DBSCAN(eps=1.5, min_samples=3)
+        dbscan.fit(data)
+        self.assertEqual(expected_labels, dbscan.labels_)
+        self.assertEqual(expected_core_sample_indices, dbscan.core_sample_indices_)
+        self.assertEqual(expected_components, dbscan.components_)
+
+    def test_get_distances_from_other_points_with_index_zero(self):
+        expected_distances_from_other_points = np.array([1.4142135, 2.8284271, 7.0710678,
+                                                         7.2111025, 7.6157731, 5.0000000])
+        self.assert_distances_almost_equal(0, expected_distances_from_other_points)
+
+    def test_get_distances_from_other_points_with_index_one(self):
+        expected_distances_from_other_points = np.array([1.4142135, 1.4142135, 7.2111025,
+                                                         7.0710678, 7.2111025, 6.0827625])
+        self.assert_distances_almost_equal(1, expected_distances_from_other_points)
+
+    def test_get_distances_from_other_points_with_index_two(self):
+        expected_distances_from_other_points = np.array([2.8284271, 1.4142136, 7.6157731,
+                                                         7.2111026, 7.0710678, 7.2801099])
+        self.assert_distances_almost_equal(2, expected_distances_from_other_points)
+
+    def test_get_distances_from_other_points_with_index_three(self):
+        expected_distances_from_other_points = np.array([7.0710678, 7.2111025, 7.6157731,
+                                                         1.4142135, 2.8284271, 5.0000000])
+        self.assert_distances_almost_equal(3, expected_distances_from_other_points)
+
+    def assert_distances_almost_equal(self, index: int, expected_distances_from_other_points):
+        distances_from_other_points = self.distances_from_other_points(index)
+        np.testing.assert_almost_equal(expected_distances_from_other_points, distances_from_other_points)
+
+    def distances_from_other_points(self, point_index: int):
+        data = self.get_two_clusters()
+        distance_matrix = pdist(data)
+        num_points = len(data)
+        return get_distances_from_other_points(point_index, num_points, distance_matrix)
 
 
 if __name__ == '__main__':
